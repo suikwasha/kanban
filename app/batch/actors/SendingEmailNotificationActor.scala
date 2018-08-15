efc8b7c511218664c52bcb66407426e57538a777
suikwasha <suikwasha@gmail.com>
2018-08-16 01:48:13 +0900

    fixed bug completed tasks deadline notification was sent

--- app/batch/actors/SendingEmailNotificationActor.scala
+++ app/batch/actors/SendingEmailNotificationActor.scala
@@ -2,11 +2,14 @@ package batch.actors
 
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
+
 import akka.actor.{Actor, Cancellable}
 import batch.notifications.EmailNotificationService
 import models.silhouette.{User, UserRepository}
+import models.task.States.Complete
 import models.task.{Task, TaskRepository}
 import org.slf4j.LoggerFactory
+
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
@@ -23,7 +26,7 @@ class SendingEmailNotificationActor(
   private[this] val logger = LoggerFactory.getLogger(getClass)
 
   val intervalTask: Cancellable =
-    context.system.scheduler.schedule(5 seconds, 1 hour, self, CheckTasks)
+    context.system.scheduler.schedule(5 seconds, 10 minutes, self, CheckTasks)
 
   override def preStart()= {
     logger.info("starting SendingEmailNotificationActor")
@@ -51,7 +54,7 @@ class SendingEmailNotificationActor(
     val now = LocalDateTime.now
     val tomorrow = now.plus(1, ChronoUnit.DAYS)
     (for {
-      tasks <- taskRepository.find(now, tomorrow).map(_.groupBy(_.author))
+      tasks <- taskRepository.find(now, tomorrow).map(_.filter(_.state != Complete).groupBy(_.author))
       users <- Future.sequence(tasks.keySet.toSeq.map(userRepository.find))
       _ <- Future.sequence(users.flatten.flatMap(u => tasks.get(u.id).map(sendNotification(u, _))))
     } yield {
